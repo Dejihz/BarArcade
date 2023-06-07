2da1e5ea48b86db3bfda261d1ac5e1a5566124dc
Dejihz <90336178+Dejihz@users.noreply.github.com>
2023-06-07 17:13:13 +0200

    nav and ui

---
+++ Quiz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games
+{
+    internal class Quiz
+    {
+        public string Category { get; set; }
+        public string Id { get; set; }
+        public string CorrectAnswer { get; set; }
+        public List<string> IncorrectAnswers { get; set; }
+        public string Question { get; set; }
+        public List<string> Tags { get; set; }
+        public string Type { get; set; }
+        public string Difficulty { get; set; }
+        public List<object> Regions { get; set; }
+        public bool IsNiche { get; set; }
+    }
+
+}
---
+++ WhackAMoleGame.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Formatting;
+
+
+namespace Games
+{
+    /// <summary>
+    /// Interaction logic for WhackAMoleGame.xaml
+    /// </summary>
+    public partial class WhackAMoleGame : Page
+    {
+        int score;
+        string question;
+        string answer;
+        string option1;
+        string option2;
+        string option3;
+        string option4;
+
+        public WhackAMoleGame()
+        {
+            InitializeComponent();
+            JsonApiCall();
+        }
+
+
+        public void setupGame()
+        {
+            score = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+
+            //this.NavigationService.Navigate(mainWindow);
+        }
+
+        private async void JsonApiCall()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var address = new Uri("https://the-trivia-api.com/api/questions/");
+                    var result = client.GetAsync(address).Result;
+                    var quiz = result.Content.ReadAsAsync<Quiz>().Result;
+
+                    //label1.Text = "Joke 1 " + joke.Jokes[0].Joke;
+
+                    Question.Content = quiz.Question;
+
+                    
+                    //System.Console.WriteLine(quiz.Question);
+                    //System.Console.WriteLine(joke.Jokes[0].Joke);
+
+                    //System.Console.WriteLine("NAmeNAmeNAmeNAmeNAmeNAmeNAmeNAmeNAmeNAme");
+                }
+            }
+            catch (AggregateException)
+            {
+            }
+        }
+    }
+}
